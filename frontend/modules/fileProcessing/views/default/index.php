0ae9ec6db51205fcb3588d35fd757a0090b04f6e
Spencer <spencerchigananda@gmail.com>
2019-02-12 20:42:42 +0200

    Renamed all the view folders to use hyphens instead of lower camel case. Made design changes to the frontend/site/index.php file so as to look different.

--- frontend/modules/fileProcessing/views/default/index.php
+++ frontend/modules/fileProcessing/views/default/index.php
@@ -1,12 +1,22 @@
 <div class="fileProcessing-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
+    <h1>CSV File Processing</h1>
+
+    <!--<h1><?/*= $this->context->action->uniqueId */?></h1>
     <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
+        This is the view content for action "<?/*= $this->context->action->id */?>".
+        The action belongs to the controller "<?/*= get_class($this->context) */?>"
+        in the "<?/*= $this->context->module->id */?>" module.
     </p>
     <p>
         You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+        <code><?/*= __FILE__ */?></code>
+    </p>-->
+
+    <ul class="nav nav-tabs">
+        <li><a href="index.php?r=fileProcessing/csvUpdate">Database record of CSV files</a></li>
+        <li><a href="index.php?r=fileProcessing/iapRevenue">Revenue</a></li>
+        <li><a href="index.php?r=fileProcessing/mnoInvestment">Interconnections</a></li>
+        <li><a href="#">Summary</a></li>
+    </ul>
+
 </div>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,19 +2,24 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Zimnat File Processing';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <div class="container">
+            <div class="row text-center">
+                <img class="center-block img-responsive" src="assets/zimnat_logo.png" style="width: 300px; height: 150px">
+            </div>
+        </div>
+        <h2>Welcome to Zimnat File Processing</h2>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p>Click on the button below to start importing CSV data into a database.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="index.php?r=fileProcessing">Get started</a></p>
     </div>
 
-    <div class="body-content">
+    <!--<div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
@@ -49,5 +54,5 @@ $this->title = 'My Yii Application';
             </div>
         </div>
 
-    </div>
+    </div>-->
 </div>
